88f039cb76d251ce22b0c5318d9320d8e85d1d76
Sneha <sneha8412@gmail.com>
2021-06-28 19:41:48 -0700

    board model populated

--- app/models/board.py
+++ app/models/board.py
@@ -1,1 +1,26 @@
 from app import db
+from flask import current_app
+
+class Board(db.Model):
+    board_id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    title = db.Colum(db.String)
+
+    def to_json_board(self):
+
+        return{
+            "board":
+            {
+                "id" :self.board_id,
+                "title": self.title      
+            }
+        }
+    
+    def to_json_board_no_key(self):
+        return {
+            "id" : self.board_id,
+            "title": self.title
+        }
+
+    @staticmethod
+    def from_json(board_json):
+        return Board(title=board_json["title"])
